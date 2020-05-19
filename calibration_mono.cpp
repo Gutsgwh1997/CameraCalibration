71c9aa6b65401151f050fe31587a55022bc36ec5
Gutsgwh1997 <guanwenhao55@163.com>
2020-05-19 21:07:59 +0800

    Add vision measurement in a plane

--- calibration_mono.cpp
+++ calibration_mono.cpp
@@ -12,6 +12,7 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/calib3d/calib3d.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
+#include <stdlib.h>
 
 using namespace std;
 using namespace cv;
@@ -22,17 +23,20 @@ class CameraCalibration {
     Size board_size_;          // 标定板上每行、列的角点数
     Size2f true_square_size_;  // 实际测量得到的标定板上每个棋盘格的大小(单位：mm)
 
-    Size image_size_;           // 棋盘格图像的像素大小
-    int num_of_sample_images_;  // 可用的棋盘格标定图像
-    vector<string> filesPath_;  // 可用的棋盘格标定图像的路径
+    Size image_size_;                  // 棋盘格图像的像素大小
+    static int num_of_sample_images_;  // 可用的棋盘格标定图像
+    vector<string> filesPath_;         // 可用的棋盘格标定图像的路径
 
-    Mat cameraMatrix_;                                // 相机内参数矩阵
-    Mat distCoeffs_;                                  // 相机畸变系数k1、k2、p1、p2、k3
-    vector<Mat> tvecsMat_;                            // 每幅图像的旋转向量
-    vector<Mat> rvecsMat_;                            // 每幅图像的平移向量
+    static Mat cameraMatrix_;                         // 相机内参数矩阵
+    static Mat distCoeffs_;                           // 相机畸变系数k1、k2、p1、p2、k3
+    static vector<Mat> tvecsMat_;                     // 每幅图像的旋转向量
+    static vector<Mat> rvecsMat_;                     // 每幅图像的平移向量
     vector<vector<Point2f>> image_points_;            // 每一副标定板上的角点坐标
     vector<vector<Point2f>> reproject_image_points_;  // 根据标定的摄像机参数重新投影的像素坐标
     vector<vector<Point3f>> object_points_;           // 每一副标定板上角点的真值三维坐标
+
+    // 视觉测量相关变量
+    static Mat sampleImage_;                          // 带有尺子的那张图像，28张里的最后一张
 
    public:
     /**
@@ -43,8 +47,8 @@ class CameraCalibration {
         board_size_       = Size(8, 6);
         true_square_size_ = Size2f(27.7667, 27.7667);
 
-        cameraMatrix_     = Mat(3, 3, CV_32FC1, Scalar::all(0));
-        distCoeffs_       = Mat(1, 5, CV_32FC1, Scalar::all(0));
+        cameraMatrix_         = Mat(3, 3, CV_64FC1, Scalar::all(0));
+        distCoeffs_           = Mat(1, 5, CV_64FC1, Scalar::all(0));
         num_of_sample_images_ = 0;
     }
 
@@ -111,18 +115,18 @@ class CameraCalibration {
             drawChessboardCorners(image, board_size_, image_points, true);
             namedWindow("Camera Calibration", WINDOW_AUTOSIZE);
             imshow("Camera Calibration", image);
-            waitKey(100);
-
+            waitKey(10);
             return true;
         }
     }
 
     void Calibration() {
         int i, j, t;
+        // 棋盘格角点是逐行，从左边到右边存储的，世界坐标系与此对应
         for(t = 0; t<num_of_sample_images_; ++t){
             vector<Point3f> tempPoints;
-            for(i = 0; i<board_size_.height; ++i){
-                for (j = 0; j<board_size_.width; ++j){
+            for (j = 0; j < board_size_.height; ++j) {
+                for (i = 0; i < board_size_.width; ++i) {
                     Point3f realPoint;
                     // 假定标定板放在世界坐标系中z=0的平面上
                     realPoint.x = i*true_square_size_.width;
@@ -165,6 +169,7 @@ class CameraCalibration {
                 --it;
             }
         }
+        destroyWindow("Camera Calibration");
         cout << image_points_.size() << " images successfully extract feature!" << endl;
 
         /*标定*/
@@ -217,16 +222,132 @@ class CameraCalibration {
             vconcat(gap_image_3,imageDistortion,gap_image_3);
             namedWindow("Undistortion image", 0);
             imshow("Undistortion image", gap_image_3);
-            waitKey(1000);
+            waitKey(200);
         }
+        cv::destroyWindow("Undistortion image");
         return;
     }
+
+    void monoMeasurement(){
+        sampleImage_ = imread(filesPath_[num_of_sample_images_-1]);
+        namedWindow("monoMeasurement", WINDOW_AUTOSIZE);
+        cv::setMouseCallback("monoMeasurement",on_mouse,0);
+        waitKey(0);
+    }
+
+    static void on_mouse(int event,int x, int y, int flages, void* ustc){
+        static char temp[16];
+        char show_distance[1024];
+        static char temp_1[16];
+        static int count = 0;
+        static Point2f firstPoint;
+        static Point2f secondPoint;
+        static Mat temp_image = sampleImage_.clone();
+        Mat temp_image_2 = sampleImage_.clone();
+        if (event == CV_EVENT_LBUTTONDOWN) {
+            ++count;
+            if (count == 1) {
+                sprintf(temp, "(%d,%d)", x, y);
+                firstPoint = Point2f(x, y);
+                putText(temp_image, temp, firstPoint, cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(0, 255, 0));
+                cv::circle(temp_image, firstPoint, 2, cv::Scalar(255, 0, 255), 2);
+            } else {
+                temp_image = sampleImage_.clone();
+                sprintf(temp, "(%d,%d)", x, y);
+                firstPoint = Point2f(x, y);
+                putText(temp_image, temp, firstPoint, cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(0, 255, 0));
+                circle(temp_image, firstPoint, 3, cv::Scalar(255, 0, 255), 3);
+                putText(temp_image, temp_1, secondPoint, cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(0, 255, 0));
+                circle(temp_image, secondPoint, 3, cv::Scalar(255, 0, 255), 3);
+                // 计算距离
+                double distance;
+                int it = num_of_sample_images_-1;
+                distance = distanceInWorld(secondPoint,firstPoint,cameraMatrix_,distCoeffs_,rvecsMat_[it],tvecsMat_[it]);
+                sprintf(show_distance,"The distance of these two poins is %f mm",distance);
+                Mat gap_image(42, temp_image.cols, CV_8UC3, cv::Scalar(255, 255, 255));
+                putText(gap_image, show_distance, Point2f(5, 34), cv::FONT_HERSHEY_SIMPLEX, 1, cv::Scalar(255, 0, 0),2);
+                vconcat(temp_image,gap_image, temp_image);
+                line(temp_image, secondPoint, firstPoint, cv::Scalar(255, 0, 0), 3, 8, 0);
+            }
+            secondPoint = firstPoint;
+            for(int i = 0; i<16 ; ++i){
+                temp_1[i] = temp[i];
+            }
+            imshow("monoMeasurement", temp_image);
+        }
+        if (event == CV_EVENT_MOUSEMOVE) {
+            temp_image_2 = temp_image.clone();
+            sprintf(temp, "(%d,%d)", x, y);
+            firstPoint = Point2f(x, y);
+            putText(temp_image_2, temp, firstPoint, cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(255, 255, 255));
+            cv::circle(temp_image_2, firstPoint, 2, cv::Scalar(255, 0, 255), 2);
+            imshow("monoMeasurement", temp_image_2);
+        }
+    }
+
+    /**
+     * @brief 计算同一平面上特征点的距离
+     *
+     * @param p1_uv 像素点1
+     * @param p2_uv 像素点2
+     * @param M     相机内参
+     * @param rvec  该图的旋转矩阵
+     * @param tvrc  该图的平移矩阵
+     * @param K     相机的畸变系数矩阵
+     *
+     * @return 世界坐标系下的距离(mm)
+     */
+    static double distanceInWorld(Point2f p1_uv, Point2f p2_uv, Mat M, Mat K, Mat rvec, Mat tvrc){
+        Mat R;
+        cv::Rodrigues(rvec,R);
+        cout<<"测量图像的旋转矩阵：\n"<<R<<endl;
+        cout<<"测量图像的平移：\n"<<tvrc<<endl;
+        // 对像素点去畸变
+        Mat puv_1_2(1,1,CV_64FC2,Scalar(p1_uv.x,p1_uv.y));
+        Mat puv_2_2(1,1,CV_64FC2,Scalar(p2_uv.x,p2_uv.y));
+        undistortPoints(puv_1_2, puv_1_2, M, K, Mat::eye(3, 3, CV_64FC1), M);
+        undistortPoints(puv_2_2, puv_2_2, M, K, Mat::eye(3, 3, CV_64FC1), M);
+
+        // 齐次像素坐标
+        Mat puv_1_3 = (cv::Mat_<double>(3, 1) << puv_1_2.at<Vec2d>(0,0).val[0], puv_1_2.at<Vec2d>(0,0).val[1],1);
+        Mat puv_2_3 = (cv::Mat_<double>(3, 1) << puv_2_2.at<Vec2d>(0,0).val[0], puv_2_2.at<Vec2d>(0,0).val[1],1);
+        
+        // 矩阵[r1,r2,t]
+        Mat R_33(3,3,CV_64FC1); 
+        R.col(0).copyTo(R_33.col(0));
+        R.col(1).copyTo(R_33.col(1));
+        tvrc.copyTo(R_33.col(2));
+
+        // 归一化相机平面坐标
+        Mat pxy_1 = M.inv() * puv_1_3;
+        Mat pxy_2 = M.inv() * puv_2_3;
+
+        // 转换为世界坐标系下的坐标
+        Mat pW_1 = R_33.inv() * pxy_1;
+        Mat pW_2 = R_33.inv() * pxy_2;
+        pW_1 = pW_1 / pW_1.at<double>(2, 0);
+        pW_2 = pW_2 / pW_2.at<double>(2, 0);
+
+        cout<<"点1在世界坐标系下的坐标是："<<pW_1.t()<<endl;
+        cout<<"点2在世界坐标系下的坐标是："<<pW_2.t()<<endl;
+        double distance = norm(pW_1, pW_2, NORM_L2);
+        cout<<"距离是："<<distance<<" mm"<<endl;
+        return distance;
+    }
 };
+
+Mat CameraCalibration::sampleImage_;
+Mat CameraCalibration::cameraMatrix_;      
+Mat CameraCalibration::distCoeffs_;        
+int CameraCalibration::num_of_sample_images_;
+vector<Mat> CameraCalibration::tvecsMat_;  
+vector<Mat> CameraCalibration::rvecsMat_;  
 
 int main(int argc, char** argv) {
     CameraCalibration cameraCalibration;
     cameraCalibration.Compute();
     cameraCalibration.AccuracyValuation();
     cameraCalibration.ShowUndistortion();
+    cameraCalibration.monoMeasurement();
     return 0;
 }
